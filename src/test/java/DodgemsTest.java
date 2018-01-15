505de61b549bafab84852767ea6d2bd62ae0c3d0
Robert Henderson <robert@nterpryz.com>
2018-01-15 17:22:13 +0000

    dodgems has price, min/max heights and min age

--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems(5, 0.80, 3.00, 8);
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(5, dodgems.getridePrice());
+    }
+
+    @Test
+    public void hasMinHeight() {
+        assertEquals(0.80, dodgems.getminHeight(), 0.01);
+    }
+
+    @Test
+    public void hasMaxHeight() {
+        assertEquals(3.00, dodgems.getmaxHeight(), 0.01);
+    }
+
+    @Test
+    public void hasMinAge() {
+        assertEquals(8, dodgems.getminAge());
+    }
 }
